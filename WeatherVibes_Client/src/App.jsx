dfb43036ca6ef156de18a80910575d4751e81d22
khushxal <vkkhushal18@gmail.com>
2024-07-01 20:55:20 +0530

    changes

--- WeatherVibes_Client/src/App.jsx
+++ WeatherVibes_Client/src/App.jsx
@@ -1,20 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 import Login from "./pages/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import About from "./pages/About";
+import Client_Home from "./pages/Client_Home";
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header></Header>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Client_Home />} />
         </Routes>
         <Footer></Footer>
       </BrowserRouter>
---
+++ WeatherVibes_Client/src/pages/Client_Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function client_home() {
+  return <div>This is client home page after login</div>;
+}
+
+export default client_home;
