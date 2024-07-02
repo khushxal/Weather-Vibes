964e5774bafa63f43c9a2936a1d3684ab7bb67e4
khushxal <vkkhushal18@gmail.com>
2024-07-02 09:48:40 +0530

    created client home page

--- WeatherVibes_Client/src/pages/Client_Home.jsx
+++ WeatherVibes_Client/src/pages/Client_Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { IoSearchCircle } from "react-icons/io5";
 function client_home() {
-  return <div>This is client home page after login</div>;
+  return (
+    <div id="container-client-home" className="container">
+      <div>
+        <button className="btn">
+          <IoSearchCircle></IoSearchCircle>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default client_home;
