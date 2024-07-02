7526fa0ba3178b21e811178c4637d16f032ae8e3
khushxal <vkkhushal18@gmail.com>
2024-07-02 10:00:10 +0530

    changes

--- WeatherVibes_Client/src/pages/Client_Home.jsx
+++ WeatherVibes_Client/src/pages/Client_Home.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoSearchCircle } from "react-icons/io5";
 function client_home() {
+  const [isClicked, setIsClicked] = useState(false);
+  function checkClick() {
+    setIsClicked();
+  }
   return (
     <div id="container-client-home" className="container">
       <div>
-        <button className="btn">
+        <button onClick={checkClick} className="btn" id="search-btn">
           <IoSearchCircle></IoSearchCircle>
         </button>
       </div>
