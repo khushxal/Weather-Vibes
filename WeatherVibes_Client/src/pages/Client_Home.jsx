bfc807e26b2973ad1e7a960ba6dce57b8e9c022d
khushxal <vkkhushal18@gmail.com>
2024-07-02 12:44:22 +0530

    search button and bar added

--- WeatherVibes_Client/src/pages/Client_Home.jsx
+++ WeatherVibes_Client/src/pages/Client_Home.jsx
@@ -1,16 +1,41 @@
 import React, { useState } from "react";
 import { IoSearchCircle } from "react-icons/io5";
+import "../css/Client_Home.css";
 function client_home() {
   const [isClicked, setIsClicked] = useState(false);
+
   function checkClick() {
-    setIsClicked();
+    setIsClicked(!isClicked);
   }
+
   return (
     <div id="container-client-home" className="container">
       <div>
-        <button onClick={checkClick} className="btn" id="search-btn">
-          <IoSearchCircle></IoSearchCircle>
-        </button>
+        <div className="row">
+          <div className="col-2 col-lg-1 me-0">
+            <button onClick={checkClick} className="btn" id="search-btn">
+              <IoSearchCircle></IoSearchCircle>
+            </button>
+          </div>
+          <div
+            id="search-bar"
+            className={`col-10 col-lg-11 d-flex ${isClicked ? "show" : ""}`}
+          >
+            {isClicked && (
+              <>
+                <input
+                  className="form-control"
+                  type="search"
+                  placeholder="Search For Music"
+                  aria-label="Search"
+                />
+                <button className="btn text-light" type="submit">
+                  Search
+                </button>
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
