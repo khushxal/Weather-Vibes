75875abfb937ed7e6ae87aa8eef5fa8b9f7fec30
Khushxal <vkkhushal18@gmail.com>
2024-07-21 20:48:07 +0530

    Changes

---
+++ WeatherVibes_Server/models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+});
+
+const user = new mongoose.model("user", userSchema);
+
+export default user;
--- WeatherVibes_Server/server.js
+++ WeatherVibes_Server/server.js
@@ -1,4 +1,7 @@
+import dot from "dotenv";
+dot.config();
 import express from "express";
+import { connectDB } from "./utils/database.js";
 import authRouter from "./routers/auth-router.js";
 import musicRouter from "./routers/music-router.js";
 
@@ -11,6 +14,8 @@ server.get("*", function (req, res) {
   res.send("<h1>Not Found 404</h1>");
 });
 
-server.listen(5000, function () {
-  console.log("Server is runnning : http://localhost:5000");
+connectDB().then(function () {
+  server.listen(5000, function () {
+    console.log(`Server is runnning : ${process.env.HOST_URI}`);
+  });
 });
