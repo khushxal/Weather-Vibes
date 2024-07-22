3acf2e4e421cc86ba3c294d4510c4119e3238b79
Khushxal <vkkhushal18@gmail.com>
2024-07-22 12:35:36 +0530

    Changes in Register File

--- WeatherVibes_Client/src/pages/Register.jsx
+++ WeatherVibes_Client/src/pages/Register.jsx
@@ -1,54 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import "../css/Register.css";
+
 function Register() {
+  const userData = {
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+    dob: "",
+  };
+
+  const [user, setUser] = useState(userData);
+
+  function handleChange(event) {
+    try {
+      const { name, value } = event.target;
+      setUser({ ...user, [name]: value });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleSubmit(event) {
+    try {
+      event.preventDefault();
+      console.log(user);
+      if (user.password === user.confirmPassword) {
+        setUser(userData);
+      } else {
+        toast.error("Please type matching password");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div id="container-register" className="container p-5 my-2">
       <div className="row">
         <div className="col-xl-6 col-sm-12">
-          <form className="m-1 p-1 fs-5">
-            <div class="form-group">
-              <label for="exampleFormControlInput1">Email address</label>
+          <form onSubmit={handleSubmit} className="m-1 p-1 fs-5">
+            <div className="form-group">
+              <label htmlFor="exampleFormControlInput1">Email address</label>
               <input
                 type="email"
-                class="form-control"
+                name="email"
+                className="form-control"
                 id="exampleFormControlInput1"
+                value={user.email}
+                onChange={handleChange}
               />
             </div>
-            <div class="form-group">
-              <label for="exampleFormControlInput1">Full Name</label>
+            <div className="form-group">
+              <label htmlFor="exampleFormControlInput1">Full Name</label>
               <input
                 type="text"
-                class="form-control"
-                id="exampleFormControlInput1"
+                name="name"
+                className="form-control"
+                id="exampleFormControlInput2"
+                value={user.name}
+                onChange={handleChange}
               />
             </div>
-            <div class="form-group">
-              <label for="exampleFormControlInput1">Password</label>
+            <div className="form-group">
+              <label htmlFor="exampleFormControlInput1">Password</label>
               <input
                 type="password"
-                class="form-control"
-                id="exampleFormControlInput1"
+                name="password"
+                className="form-control"
+                id="exampleFormControlInput3"
+                value={user.password}
+                onChange={handleChange}
               />
             </div>
-            <div class="form-group">
-              <label for="exampleFormControlInput1">Confirm Password</label>
+            <div className="form-group">
+              <label htmlFor="exampleFormControlInput1">Confirm Password</label>
               <input
                 type="password"
-                class="form-control"
-                id="exampleFormControlInput1"
+                name="confirmPassword"
+                className="form-control"
+                id="exampleFormControlInput4"
+                value={user.confirmPassword}
+                onChange={handleChange}
               />
             </div>
-            <div class="form-group">
-              <label for="exampleFormControlInput1">DoB</label>
+            <div className="form-group">
+              <label htmlFor="exampleFormControlInput1">DoB</label>
               <input
                 type="date"
-                class="form-control"
-                id="exampleFormControlInput1"
+                name="dob"
+                className="form-control"
+                id="exampleFormControlInput5"
+                value={user.dob}
+                onChange={handleChange}
               />
             </div>
             <div className="text-center mt-2">
-              <button className="btn">Get Started</button>
+              <button className="btn text-white">Get Started</button>
               <hr />
               <p>
                 Not having account ? <Link to="/register">Sign Up</Link>
